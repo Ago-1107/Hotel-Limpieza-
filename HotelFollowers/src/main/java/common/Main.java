796af56852ed83ba9c4f7d8a3ebe34583143860a
Andrea González Ochoa <144396134+Ago-1107@users.noreply.github.com>
2024-03-23 23:50:28 -0500

    Add files via upload

---
+++ HotelFollowers/src/main/java/common/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package common;
+
+import static housekeeping.Record.cleanRoom;
+
+/**
+ *
+ * @author Andrea
+ */
+public class Main {
+    public static void main(String[] args) {       
+        cleanRoom();
+    }
+}
